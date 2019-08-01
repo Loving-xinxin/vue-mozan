00fe5e5bf45659d36956fddb9d183c120c111c4e
Loving-xinxin <2603712235@qq.com>
2019-08-01 12:18:42 +0800

    veee

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="wrap">
+    <div class="inner">
+      <Header />
+
+      <router-view></router-view>
+
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+export default {
+  name: "app",
+  components: {
+    Header,
+    Main,
+    Footer
+  }
+};
+</script>
+
+<style>
+.wrap {
+  margin: 0;
+  padding: 0;
+}
+</style>
