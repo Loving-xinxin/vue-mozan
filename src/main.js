676ac3b0be1228a7d7ba04aa1acb8f9a24eb61a8
Loving-xinxin <2603712235@qq.com>
2019-08-01 17:34:29 +0800

    jdjeiji

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import 'element-ui/lib/theme-chalk/index.css';
 import VueAwesomeSwiper from 'vue-awesome-swiper';
 import 'swiper/dist/css/swiper.css';
 import router from './router';
+import { PUBLICURL } from './publicurl';
+Vue.prototype.$publicUrl = PUBLICURL;
 Vue.use(VueAwesomeSwiper);
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
